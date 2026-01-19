3f6426354de5a84e83b6c1074aa60750c33a556c
zfrye151 <zachary.frye@csuglobal.edu>
2026-01-18 23:50:53 -0600

    Initial commit
    Initial commit for my courses

---
+++ CSC 500/Module 1/Assignment1_part1_ZacharyFrye.py
@@ -0,0 +1,13 @@
+# Zachary Frye
+# CSC 500
+# Assignment 1 - Part 1
+
+print('Hello! Please input a number:') 
+user_num1 = int(input()) #prompt and get first number
+print('Please input a second number:')
+user_num2 = int(input()) #prompt and get second number
+print()
+sum = user_num1 + user_num2 #sum two numbers together
+diff = user_num1 - user_num2 #subtract two numbers
+print('The sum of', user_num1, 'and', user_num2, 'is:', sum) # output results
+print('The difference of', user_num1, 'and', user_num2, 'is:', diff)
---
+++ CSC 500/Module 1/Assignment1_part2_ZacharyFrye.py
@@ -0,0 +1,13 @@
+# Zachary Frye
+# CSC 500
+# Assignment 1 - Part 2
+
+print('Hello! Please input a number:') 
+user_num1 = int(input()) #prompt and get first number
+print('Please input a second number:')
+user_num2 = int(input()) #prompt and get second number
+print()
+prod = user_num1 * user_num2 #sum two numbers together
+div = user_num1 / user_num2 #subtract two numbers
+print('The product of', user_num1, 'and', user_num2, 'is:', prod) # output results
+print('The division of', user_num1, 'and', user_num2, 'is:', div)
